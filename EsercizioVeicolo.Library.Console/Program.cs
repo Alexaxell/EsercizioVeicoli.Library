e8a857e24c7e3059547651eac6e21ac9d20509ea
10228 <nikolovalex08@gmail.com>
2026-01-15 08:31:26 +0100

    aa

--- EsercizioVeicolo.Library.Console/Program.cs
+++ EsercizioVeicolo.Library.Console/Program.cs
@@ -6,53 +6,79 @@ namespace EsercizioVeicolo.Library.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Inserisci il tipo di veicolo desiderato: ");
-            string TipoVeicolo = System.Console.ReadLine();
+            var listAutoveicoli = new List<AutoVeicolo>();
+            var listaAutocarri = new List<AutoCarro>();
 
-            if (TipoVeicolo.Equals("Autoveicolo", StringComparison.CurrentCultureIgnoreCase))
+            string scelta;
+
+            while (true)
             {
-                System.Console.WriteLine("Inserisci il numero di targa: ");
-                string targa = System.Console.ReadLine();
-                System.Console.WriteLine("Inserisci la marca del veicolo: ");
-                string marca = System.Console.ReadLine();
-                System.Console.WriteLine("Inserisci il numero di chilometri percorsi: ");
-                double kmPercorsi = double.Parse(System.Console.ReadLine());
-                System.Console.WriteLine("Inserisci il numero di litri di carburante consumati: ");
-                double litriCarburanteConsumato = double.Parse(System.Console.ReadLine());
-                System.Console.WriteLine("Inserisci il numero di posti dell'autoveicolo: ");
-                int numeroPosti = int.Parse(System.Console.ReadLine());
+                System.Console.WriteLine("1. Inserisci un autoveicolo.");
+                System.Console.WriteLine("2. Inserisci un autocarro.");
+                System.Console.WriteLine("3. Stampa un autoveicolo a scelta.");
+                System.Console.WriteLine("3. stampa un autocarro a scelta.");
+                System.Console.WriteLine("3. Esci.");
+                scelta = System.Console.ReadLine();
 
-                var autoveicolo = new AutoVeicolo()
-                {
-                    Targa = targa,
-                    Marca = marca,
-                    KmPercorsi = kmPercorsi,
-                    LitriCarburanteConsumato = litriCarburanteConsumato,
-                    NumeroPosti = numeroPosti
-                };
-            }
-            else if (TipoVeicolo.Equals("Autocarro", StringComparison.CurrentCultureIgnoreCase))
-            {
-                System.Console.WriteLine("Inserisci il numero di targa: ");
-                string targa = System.Console.ReadLine();
-                System.Console.WriteLine("Inserisci la marca del veicolo: ");
-                string marca = System.Console.ReadLine();
-                System.Console.WriteLine("Inserisci il numero di chilometri percorsi: ");
-                double kmPercorsi = double.Parse(System.Console.ReadLine());
-                System.Console.WriteLine("Inserisci il numero di litri di carburante consumati: ");
-                double litriCarburanteConsumato = double.Parse(System.Console.ReadLine());
-                System.Console.WriteLine("Inserisci la capacità del carico di tonnellate dell'autocarro: ");
-                double capacitaCaricoTonnellate = double.Parse(System.Console.ReadLine());
+                string marca;
+                string targa;
+                int numeroPosti;
+                double kmPercorsi;
+                double litriCarburanteConsumato;
 
-                var autocarro = new AutoCarro()
+                switch (scelta)
                 {
-                    Targa = targa,
-                    Marca = marca,
-                    KmPercorsi = kmPercorsi,
-                    LitriCarburanteConsumato = litriCarburanteConsumato,
-                    CapacitaCaricoTonnelate = capacitaCaricoTonnellate
-                };
+                    case "1":
+                        System.Console.WriteLine("Inserisci il numero di targa: ");
+                        targa = System.Console.ReadLine();
+                        System.Console.WriteLine("Inserisci la marca del veicolo: ");
+                        marca = System.Console.ReadLine();
+                        System.Console.WriteLine("Inserisci il numero di chilometri percorsi: ");
+                        kmPercorsi = double.Parse(System.Console.ReadLine());
+                        System.Console.WriteLine("Inserisci il numero di litri di carburante consumati: ");
+                        litriCarburanteConsumato = double.Parse(System.Console.ReadLine());
+                        System.Console.WriteLine("Inserisci il numero di posti dell'autoveicolo: ");
+                        numeroPosti = int.Parse(System.Console.ReadLine());
+
+                        listAutoveicoli.Add(new AutoVeicolo
+                        {
+                            Targa = targa,
+                            Marca = marca,
+                            KmPercorsi = kmPercorsi,
+                            LitriCarburanteConsumato = litriCarburanteConsumato,
+                            NumeroPosti = numeroPosti
+                        });
+
+                        break;
+
+                    case "2":
+                        System.Console.WriteLine("Inserisci il numero di targa: ");
+                        targa = System.Console.ReadLine();
+                        System.Console.WriteLine("Inserisci la marca del veicolo: ");
+                        marca = System.Console.ReadLine();
+                        System.Console.WriteLine("Inserisci il numero di chilometri percorsi: ");
+                        kmPercorsi = double.Parse(System.Console.ReadLine());
+                        System.Console.WriteLine("Inserisci il numero di litri di carburante consumati: ");
+                        litriCarburanteConsumato = double.Parse(System.Console.ReadLine());
+                        System.Console.WriteLine("Inserisci la capacità del carico di tonnellate dell'autocarro: ");
+                        double capacitaCaricoTonnellate = double.Parse(System.Console.ReadLine());
+
+                        listaAutocarri.Add(new AutoCarro
+                        {
+                            Targa = targa,
+                            Marca = marca,
+                            KmPercorsi = kmPercorsi,
+                            LitriCarburanteConsumato = litriCarburanteConsumato,
+                            CapacitaCaricoTonnelate = capacitaCaricoTonnellate
+                        });
+
+                        break;
+
+                    case "3":
+                        return;
+                }
             }
         }
     }
 }
+
